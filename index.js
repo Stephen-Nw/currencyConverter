70601c70ea4be427739b6bd8bea3d404794b446e
Stephen <interprimos80@hotmail.com>
2024-08-21 21:16:16 -0500

    create url for currency exchange

--- index.js
+++ index.js
@@ -8,32 +8,37 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const BASE_URL = "https://v6.exchangerate-api.com/v6";
+const BASE_URL = "https://v6.exchangerate-api.com/v6/";
 const API_KEY = process.env.EXCHANGE_RATE_API_KEY;
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
 });
 
-app.post("/submit", (req, res) => {
+app.post("/submit", async (req, res) => {
     const yourCountry = (req.body["yourCountry"]);
     const foreignCountry = (req.body["foreignCountry"]);
-    const yourAmount = parseInt(req.body["yourAmount"]);
+
+    const yourAmount = parseInt(req.body["yourAmount"]);  
+    const foreignAmount = calculateCurrency(yourAmount, 10);
+
     const baseAmount = 1;
-    const baseCurrency = (req.body["baseCurrency"]);
-    const exchangeValue = calculateCurrency(yourAmount, 10);
-    const exchangeCurrency = (req.body["exchangeCurrency"]);
-    const yourCurrency = currencySymbol(yourCountry);
-    const foreignCurrency = currencySymbol(foreignCountry);
-    // console.log(`Your Country: ${yourCountry}`);
-    // console.log(`Foreign Country: ${foreignCountry}`);
-    // console.log(`Your Amount: ${yourAmount}`);
-    // console.log(`Exchange Rate: ${baseAmount}`);
-    // console.log(`Exchange Currency: ${baseCurrency}`);
-    console.log(`Exchange Value: ${exchangeValue}`);
-    console.log(`Your Currency: ${yourCurrency}`);
-    console.log(`Foreign Currency: ${foreignCurrency}`);
-    res.render("index.ejs")
+    const baseCurrency = currencySymbol(yourCountry);
+
+    // const exchangeAmount = 
+    const exchangeCurrency = currencySymbol(foreignCountry);
+
+    try {
+        const response = await axios.get(BASE_URL + API_KEY + "/pair/" + baseCurrency + "/" + exchangeCurrency + "/" + yourAmount);
+        const result = JSON.stringify(response.data);
+        console.log(result);
+        res.render("index.ejs");
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+    }
 });
 
 // Calculate value of foreign currency given base currency and exchange rate
